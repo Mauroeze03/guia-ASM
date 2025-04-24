6631f0ffe0cd5a7c61b40430380aa4fd4bc58214
mauroeze03 <maurod200304@gmail.com>
2025-04-23 22:33:52 -0300

    a

--- src/2 - ABI/ABI.asm
+++ src/2 - ABI/ABI.asm
@@ -97,45 +97,132 @@ alternate_sum_4_using_c_alternative:
 
 
 ; uint32_t alternate_sum_8(uint32_t x1, uint32_t x2, uint32_t x3, uint32_t x4, uint32_t x5, uint32_t x6, uint32_t x7, uint32_t x8);
-; registros y pila: x1[?], x2[?], x3[?], x4[?], x5[?], x6[?], x7[?], x8[?]
+; registros y pila: x1[EDI], x2[ESI], x3[EDX], x4[ECX], x5[r8], x6[r9], x7[rbp+16], x8[rbp+24 ]
 alternate_sum_8:
 	;prologo
+  push RBP
+  mov RBP,RSP
+  push R12
+  push R13
+  push R14
+  push R15
 
+  mov R12D,EDX
+  mov R13D, ECX
+  mov R14D, R8D
+  mov R15D, R9D
 	; COMPLETAR
+  call restar_c
 
+  mov EDI, EAX
+  mov ESI, R12D
+
+  call sumar_c
+
+  mov EDI, EAX
+  mov ESI, r13D
+
+  call restar_c
+
+  mov EDI,EAX
+  mov ESI, R14D
+
+  call sumar_c
+
+  mov EDI, EAX
+  mov ESI, R15D
+
+  call restar_c
+
+  mov EDI, EAX
+  mov ESI, [RBP+16]
+
+  call sumar_c
+  mov EDI, EAX
+  mov ESI, [RBP+24]
+  call restar_c
 	;epilogo
+  pop R12
+  pop R13
+  pop R14
+  pop R15
+  pop RBP
 	ret
 
 
 ; SUGERENCIA: investigar uso de instrucciones para convertir enteros a floats y viceversa
 ;void product_2_f(uint32_t * destination, uint32_t x1, float f1);
-;registros: destination[?], x1[?], f1[?]
+;registros: destination[RDI], x1[ESI], f1[xmm0]
 product_2_f:
-	ret
+ push rbp
+  mov rbp, rsp
+
+  cvtsi2sd xmm1, ESI
+  cvtss2sd xmm0, xmm0
+  mulsd xmm0, xmm1
+  cvttsd2si ESI, xmm0
+  mov [RDI], ESI
+
+  pop rbp
+  ret
+
 
 
 ;extern void product_9_f(double * destination
 ;, uint32_t x1, float f1, uint32_t x2, float f2, uint32_t x3, float f3, uint32_t x4, float f4
 ;, uint32_t x5, float f5, uint32_t x6, float f6, uint32_t x7, float f7, uint32_t x8, float f8
 ;, uint32_t x9, float f9);
-;registros y pila: destination[rdi], x1[?], f1[?], x2[?], f2[?], x3[?], f3[?], x4[?], f4[?]
-;	, x5[?], f5[?], x6[?], f6[?], x7[?], f7[?], x8[?], f8[?],
-;	, x9[?], f9[?]
+;registros y pila: destination[RDI], x1[ESI], f1[XMM0], x2[EDX], f2[XMM1], x3[EDC], f3[XMM2], x4[R8], f4[XMM3]
+;	, x5[R9], f5[XMM4], x6[RBP+16], f6[XMM5], x7[RBP+24], f7[XMM6], x8[RSP+32], f8[XMM7],
+;	, x9[RSP+40], f9[RSP+48]
 product_9_f:
-	;prologo
-	push rbp
-	mov rbp, rsp
+    push rbp
+    mov rbp, rsp
 
-	;convertimos los flotantes de cada registro xmm en doubles
-	; COMPLETAR
+    cvtss2sd xmm0, xmm0
+    cvtss2sd xmm1, xmm1
+    cvtss2sd xmm2, xmm2
+    cvtss2sd xmm3, xmm3
+    cvtss2sd xmm4, xmm4
+    cvtss2sd xmm5, xmm5
+    cvtss2sd xmm6, xmm6
+    cvtss2sd xmm7, XMM7
 
-	;multiplicamos los doubles en xmm0 <- xmm0 * xmm1, xmmo * xmm2 , ...
-	; COMPLETAR
+    mulsd xmm0, xmm1
+    mulsd xmm0, xmm2
+    mulsd xmm0, xmm3
+    mulsd xmm0, xmm4
+    mulsd xmm0, xmm5
+    mulsd xmm0, xmm6
+    mulsd xmm0, xmm7
 
-	; convertimos los enteros en doubles y los multiplicamos por xmm0.
-	; COMPLETAR
+    movss xmm1, [RSP+48]
+    cvtss2sd xmm1, xmm1
+    mulsd xmm0, xmm1 ; multiplicados todos los f en xmm0 pasados a double
 
-	; epilogo
-	pop rbp
-	ret
+    cvtsi2sd xmm1,ESI ;x1
+    cvtsi2sd xmm2,EDX ;x2
+    cvtsi2sd xmm3,ECX ;x3
+    cvtsi2sd xmm4,R8 ;x4
+    cvtsi2sd xmm5,R9 ;x5
+    cvtsi2sd xmm6,[RBP+16] ;x6
+    cvtsi2sd xmm7,[RBP+24] ;x7 ; convertidos todos los x a double y alojados en registros xmm
 
+    mulsd xmm0, xmm1
+    mulsd xmm0, xmm2
+    mulsd xmm0, xmm3
+    mulsd xmm0, xmm4
+    mulsd xmm0, xmm5
+    mulsd xmm0, xmm6
+    mulsd xmm0, xmm7
+
+    cvtsi2sd xmm1,[RBP+32] ;x8
+    cvtsi2sd xmm2,[RBP+40] ;x9
+
+    mulsd xmm0, xmm1
+    mulsd xmm0, xmm2 ; multiplicados todos los x int en xmm0 con el res de los float   
+
+    movsd [rdi], xmm0
+
+    pop rbp
+    ret
--- src/3 - Alineación y Estructuras/Estructuras.asm
+++ src/3 - Alineación y Estructuras/Estructuras.asm
@@ -2,16 +2,16 @@
 
 ;########### ESTOS SON LOS OFFSETS Y TAMAÑO DE LOS STRUCTS
 ; Completar las definiciones (serán revisadas por ABI enforcer):
-NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_NEXT EQU 0
 NODO_OFFSET_CATEGORIA EQU 8
-NODO_OFFSET_ARREGLO EQU 8
-NODO_OFFSET_LONGITUD EQU 8
-NODO_SIZE EQU 8
-PACKED_NODO_OFFSET_NEXT EQU 8
+NODO_OFFSET_ARREGLO EQU 16
+NODO_OFFSET_LONGITUD EQU 24
+NODO_SIZE EQU 32
+PACKED_NODO_OFFSET_NEXT EQU 0
 PACKED_NODO_OFFSET_CATEGORIA EQU 8
-PACKED_NODO_OFFSET_ARREGLO EQU 8
-PACKED_NODO_OFFSET_LONGITUD EQU 8
-PACKED_NODO_SIZE EQU 8
+PACKED_NODO_OFFSET_ARREGLO EQU 9
+PACKED_NODO_OFFSET_LONGITUD EQU 17
+PACKED_NODO_SIZE EQU 21
 LISTA_OFFSET_HEAD EQU 8
 LISTA_SIZE EQU 8
 PACKED_LISTA_OFFSET_HEAD EQU 8
@@ -29,12 +29,34 @@ global cantidad_total_de_elementos_packed
 
 ;########### DEFINICION DE FUNCIONES
 ;extern uint32_t cantidad_total_de_elementos(lista_t* lista);
-;registros: lista[?]
+;registros: lista[RDI]
 cantidad_total_de_elementos:
+	push RBP
+	mov RBP,RSP
+	mov RDI,[RDI]; voy de lista_t* lista a nodo_t* head
+	mov EAX,0
+	loop:
+		add EAX,[RDI+24]; voy de nodo_t* head a su atributo longitud
+		mov RDI,[RDI]; voy de nodo_t* head a nodo_t* next
+		cmp RDI,0; comparo si el siguiente nodo es null
+		jne loop; si no es null sigo el loop
+
+	pop RBP
 	ret
 
 ;extern uint32_t cantidad_total_de_elementos_packed(packed_lista_t* lista);
-;registros: lista[?]
+;registros: lista[RDI]
 cantidad_total_de_elementos_packed:
+	push RBP
+	mov RBP,RSP
+	mov RDI,[RDI];voy de packed_lista_t* lista a nodo_t* head
+	mov EAX,0
+	loop2:
+		add EAX,[RDI+17]; voy de packed_nodo_t* head a su atributo longitud
+		mov RDI,[RDI]; voy de packed_nodo_t* head a packed
+		cmp RDI,0; comparo si el siguiente nodo es null
+		jne loop2; si no es null sigo el loop
+	pop RBP
+
 	ret
 
